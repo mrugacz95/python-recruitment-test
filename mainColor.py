eee9c8b0fadb36104ceffe9a0bd0f86332686503
m.mrugas@itgenerator.pl <ITgen4ever>
2017-03-09 00:32:19 +0100

    code for python 2.7

--- mainColor.py
+++ mainColor.py
@@ -1,41 +1,47 @@
+from __future__ import print_function
 import glob
-from typing import Tuple
 import re
-import cv2
 import numpy as np
+from skimage import color
+from skimage import io
 
 
-def categoryColor(color: Tuple):
-    h = color[0] / 255
-    s = color[1] / 255
-    v = color[2] / 255
+def categoryColor(color):
+    h = color[0]
+    s = color[1]
+    v = color[2]
     if s < 0.5:
         if v < 0.5:
             return 0
         else:
             return 1
-    val = int(round(h * 6)) % 6 + 2
-    return val
+    return int(round(h * 6)) % 6 + 2
 
-def findDominantColor(image: np.ndarray) -> str:
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV_FULL)
+
+def findDominantColor(image):
+    if image.shape[2] > 3:  # remove alpha
+        image = image[:, :, :3]
+    image = color.rgb2hsv(image)
     colors = ("black", "white", "red", "yellow", "green", "cyan", "blue", "magenta")
+    image = np.array(image)
     col = np.zeros(8)
     image = np.reshape(image, (np.product(image.shape[0:2]), 3))
-    for color in image:
-        col[categoryColor(color)] += 1
+    for c in image:
+        col[categoryColor(c)] += 1
     return colors[np.argmax(col)]
 
 
 def main():
     for file in glob.glob("./*.png"):
-        image = cv2.imread(file)
+        image = io.imread(file)
         print("File " + file)
         domainColor = findDominantColor(image)
         print("Dominant color: " + domainColor)
         if re.search(domainColor, file, re.IGNORECASE):
             print("OK")
-        print("\n",end="")
+        else:
+            print("ERROR")
+        print("\n", end='')
 
 
 if __name__ == '__main__':
